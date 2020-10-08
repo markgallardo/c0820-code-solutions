39e15c05a6936cb14baccab250a047e331149d30
markgallardo <markgallardo1221@gmail.com>
2020-10-08 11:43:41 -0700

    complete node module system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+console.log(typeof exports);
+console.log(exports);
+console.log(typeof require);
+console.log(require);
+console.log(typeof module);
+console.log(module);
+console.log(typeof _filename);
+console.log(__filename);
+console.log(typeof _dirname);
+console.log(__dirname);
