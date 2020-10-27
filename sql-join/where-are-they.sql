bb2821a25988cfba5e0c0df687b23715c69e22e7
markgallardo <markgallardo1221@gmail.com>
2020-10-27 15:05:14 -0700

    revise on where are they

--- sql-join/where-are-they.sql
+++ sql-join/where-are-they.sql
@@ -1,6 +1,6 @@
 select  "a"."line1",
        "a"."district",
-       "c"."name" as "city"
+       "c"."name" as "city",
        "C"."name" as "country"
 from "addresses" as "a"
 join  "cities" as "c" using ("cityId")
